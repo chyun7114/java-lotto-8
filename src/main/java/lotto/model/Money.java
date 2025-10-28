6762b342e35b76735b06c85827183dfc51111654
chyun7114 <chyun7114@naver.com>
2025-10-28 20:14:28 +0900

    feat: 로또 구매를 구현하기 위한 도메인을 정의한다 - 돈을 나타내는 Money 래퍼 객체 생성 - 구매의 행위를 하는 User 객체 생성 - 로또 목록을 나타내는 Lottos 객체 생성

---
+++ src/main/java/lotto/model/Money.java
@@ -0,0 +1,14 @@
+package lotto.model;
+
+public class Money {
+
+    private final int amount;
+
+    public Money(int amount) {
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return this.amount;
+    }
+}
---
+++ src/main/java/lotto/model/User.java
@@ -0,0 +1,12 @@
+package lotto.model;
+
+public class User {
+
+    private final Lottos lottos;
+    private final Money money;
+
+    public User(Lottos lottos, Money money) {
+        this.lottos = lottos;
+        this.money = money;
+    }
+}
