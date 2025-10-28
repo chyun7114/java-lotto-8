82bb0be9db46a7c82eafbb8be261b6953a26699e
chyun7114 <chyun7114@naver.com>
2025-10-28 23:45:37 +0900

    chore: LottoTest 패키지 변경

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,8 +1,7 @@
-package lotto;
+package lotto.model;
 
 import java.util.stream.Stream;
 import lotto.exception.LottoErrorCode;
-import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
